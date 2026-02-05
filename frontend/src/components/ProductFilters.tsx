9b6dd0657834738812fa44f78af3fec6518c3d61
Kalany <duncan.miard@outlook.fr>
2026-02-05 08:03:39 +0100

    Minor graphical change in ProductFilters

--- frontend/src/components/ProductFilters.tsx
+++ frontend/src/components/ProductFilters.tsx
@@ -30,7 +30,7 @@ export default function ProductFilters({
               className="input-field pl-10"
             />
             <svg
-              className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400"
+              className="absolute right-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400"
               fill="none"
               stroke="currentColor"
               viewBox="0 0 24 24"
